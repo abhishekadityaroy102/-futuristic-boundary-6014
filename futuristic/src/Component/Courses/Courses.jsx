943e1cc7710581b477b35692a68b0a17e79aa941
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-29 04:37:16 +0530

    Update Courses.jsx

--- futuristic/src/Component/Courses/Courses.jsx
+++ futuristic/src/Component/Courses/Courses.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "../../Pages/Footer";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Courses;
