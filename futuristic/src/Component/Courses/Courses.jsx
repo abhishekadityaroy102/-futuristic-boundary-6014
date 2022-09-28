e0f89d84ab6d0f4e9806247aea0a239c4b192ec2
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-28 17:32:01 +0530

    Update Courses.jsx

--- futuristic/src/Component/Courses/Courses.jsx
+++ futuristic/src/Component/Courses/Courses.jsx
@@ -1,7 +1,483 @@
 import React from "react";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      <div class="footer_footerWrap__CCs3c">
+        <div class="container">
+          <footer class="footer_footer__iuoCa">
+            <div class="footer_footerCol__kqvec">
+              <a class="footer_footerLogo__2lk68" href="index.html">
+                <span
+                  style="
+                    box-sizing: border-box;
+                    display: block;
+                    overflow: hidden;
+                    width: initial;
+                    height: initial;
+                    background: none;
+                    opacity: 1;
+                    border: 0;
+                    margin: 0;
+                    padding: 0;
+                    position: relative;
+                  "
+                >
+                  <span
+                    style="
+                      box-sizing: border-box;
+                      display: block;
+                      width: initial;
+                      height: initial;
+                      background: none;
+                      opacity: 1;
+                      border: 0;
+                      margin: 0;
+                      padding: 0;
+                      padding-top: 66.21621621621621%;
+                    "
+                  ></span>
+                  <img
+                    alt="shine"
+                    src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                    decoding="async"
+                    data-nimg="responsive"
+                    class="img-fluid"
+                    style="
+                      position: absolute;
+                      top: 0;
+                      left: 0;
+                      bottom: 0;
+                      right: 0;
+                      box-sizing: border-box;
+                      padding: 0;
+                      border: none;
+                      margin: auto;
+                      display: block;
+                      width: 0;
+                      height: 0;
+                      min-width: 100%;
+                      max-width: 100%;
+                      min-height: 100%;
+                      max-height: 100%;
+                    "
+                  />
+                  <noscript>
+                    <img
+                      alt="shine"
+                      sizes="100vw"
+                      srcset="
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=640&amp;q=75   640w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=750&amp;q=75   750w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=828&amp;q=75   828w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=1080&amp;q=75 1080w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=1200&amp;q=75 1200w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=1920&amp;q=75 1920w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=2048&amp;q=75 2048w,
+                        /_next/image?url=%2Fimages%2Fshine-logo.png&amp;w=3840&amp;q=75 3840w
+                      "
+                      src="_next/shine-logo10dc.png?url=%2Fimages%2Fshine-logo.png&amp;w=3840&amp;q=75"
+                      decoding="async"
+                      data-nimg="responsive"
+                      style="
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        bottom: 0;
+                        right: 0;
+                        box-sizing: border-box;
+                        padding: 0;
+                        border: none;
+                        margin: auto;
+                        display: block;
+                        width: 0;
+                        height: 0;
+                        min-width: 100%;
+                        max-width: 100%;
+                        min-height: 100%;
+                        max-height: 100%;
+                      "
+                      class="img-fluid"
+                      loading="lazy"
+                    />
+                  </noscript>
+                </span>
+              </a>
+              <p class="font-size-16">Great career starts here!</p>
+              <span class="footer_appStore__f2jgh">
+                <strong>Download Shine App</strong>
+                <span class="footer_appStoreIocnWrap__Kvxc_">
+                  <a href="https://play.google.com/store/apps/details?id=com.net.shine">
+                    <i class="footer_iconGooglePlayStore__7AtZw"></i>
+                  </a>
+                  <a href="https://apps.apple.com/in/app/shine-com-job-search/id950558510">
+                    <i class="footer_iconApplePlayStore__BpEtd"></i>
+                  </a>
+                </span>
+              </span>
+            </div>
+            <div class="footer_footerCol__kqvec">
+              <h3>Job Seekers</h3>
+              <ul>
+                <li>
+                  <a href="register/index.html">Register</a> /
+                  <a href="login/index.html">Login</a>
+                </li>
+                <li>
+                  <a href="https://www.shine.com/job-search/">Browse Jobs</a>
+                </li>
+                <li>
+                  <a href="https://www.shine.com/free-job-alerts/">
+                    Create Free Job Alert
+                  </a>
+                </li>
+                <li>
+                  <a href="https://resume.shine.com/">
+                    Job Assistance Services
+                  </a>
+                </li>
+                <li>
+                  <a href="index.html">Courses</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer_footerCol__kqvec">
+              <h3>Employers</h3>
+              <ul>
+                <li>
+                  <a href="https://recruiter.shine.com/?utm_source=shine&amp;utm_medium=footer&amp;utm_campaign=Candidate">
+                    Recruiter India
+                  </a>
+                </li>
+                <li>
+                  <a href="https://recruiter.shine.com/online-package/offer/free-job-posting/">
+                    Post Job
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer_footerCol__kqvec">
+              <h3>Partner Sites</h3>
+              <ul>
+                <li>
+                  <a href="https://www.englishmate.com/">English Mate</a>
+                </li>
+                <li>
+                  <a href="https://www.studymateonline.com/">Study Mate</a>
+                </li>
+                <li>
+                  <a href="https://www.hindustantimes.com/">
+                    Hindustantimes.com
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.livemint.com/">Livemint.com</a>
+                </li>
+                <li>
+                  <a href="https://www.livehindustan.com/">Livehindustan.com</a>
+                </li>
+                <li>
+                  <a href="https://www.ottplay.com/?utm_source=shine&amp;utm_medium=partner-site&amp;utm_campaign=april-21">
+                    OTTplay.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer_footerCol__kqvec">
+              <h3>Contact Us</h3>
+              <ul>
+                <li>
+                  <a href="tel:080-4710 5555">080-4710 5555</a>
+                </li>
+                <li>
+                  <a href="mailto:contactus@shine.com">contactus@shine.com</a>
+                </li>
+                <li class="footer_smo__pBZBF">
+                  <a
+                    href="https://www.linkedin.com/company/shinecom/"
+                    class="footer_smoLinkedin__oGBUw"
+                  ></a>
+                  <a
+                    href="https://twitter.com/Shinedotcom?lang=en"
+                    class="footer_smotwitter__NgU6V"
+                  ></a>
+                  <a
+                    href="https://www.facebook.com/shinedotcom/"
+                    class="footer_smofacebook__x4sX3"
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+        </div>
+        <div class="footer_footerBottomWrap__S_1iV">
+          <div class="container">
+            <div class="footer_footerBottom__vywfZ">
+              <div class="footer_footerBottomLeft__YP_n0">
+                <a href="https://www.shine.com/faqs/">FAQs</a>
+                <a href="https://www.shine.com/aboutus/">About Us</a>
+                <a href="contact-us.html">Contact Us</a>
+                <a href="privacy-policy/index.html">Privacy Policy</a>
+                <a href="https://www.shine.com/cookie-policy/">Cookie Policy</a>
+                <a href="https://www.shine.com/securityadvice/">Fraud Alert</a>
+                <a href="https://www.livemint.com/">Business News</a>
+                <a href="https://www.hindustantimes.com/">English News</a>
+                <a href="tnc/index.html">Terms &amp; Conditions</a>
+                <a href="disclaimer/index.html">Disclaimer</a>
+                <a href="https://www.shine.com/contactus/?type=reportJobPosting/">
+                  Report a Job Posting
+                </a>
+              </div>
+              <div class="footer_footerBottomRight__ZiNIS">
+                ©{/* <!-- -->2022<!-- --> */}
+                HT Media Limited
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul class="mobileBottomNav_headNav__W4mPz">
+        <li class="">
+          <a href="https://www.shine.com/">
+            <span class="mobileBottomNav_mfnHome__hTZDI">Home</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="https://www.shine.com/job-search/">
+            <span class="mobileBottomNav_mfnJobs__VO3Qa">Jobs</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="https://resume.shine.com/">
+            <span class="mobileBottomNav_mfnServices__F6iv3">Services</span>
+          </a>
+        </li>
+        <li class="mobileBottomNav_active___VHGd">
+          <a href="index.html">
+            <span class="mobileBottomNav_mfnCourses__DVDD_">Courses</span>
+          </a>
+        </li>
+        <li class="">
+          <a href="https://www.shine.com/myshine/login/">
+            <span class="mobileBottomNav_mfnProfile__Pz34u">Profile</span>
+          </a>
+        </li>
+      </ul>
+      <div class="slideMenuWrap">
+        <nav class="slideMenu">
+          <div class="slideMenu__header">
+            <div class="slideMenu__header--info">
+              <strong>Welcome!</strong>
+              <div class="d-flex mt-10">
+                <a href="login/index.html">
+                  <button class="btn btn-outline-primary mr-15">Sign in</button>
+                </a>
+                <a href="register/index.html">
+                  <button class="btn btn-outline-primary">Register</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <ul class="tabs">
+            <li class="tabs--heading">
+              <a href="https://www.shine.com/">
+                <i class="iconH-home"></i>Home
+              </a>
+            </li>
+            <li>
+              <a class="tab-link" href="https://www.shine.com/">
+                <span class="tab-label">
+                  <i class="iconH-job"></i>Jobs
+                </span>
+              </a>
+              <input type="checkbox" id="chck1" checked="" />
+              <label class="tab-arrow" for="chck1"></label>
+              <ul class="tab-content">
+                <li class="tab">
+                  <a href="https://www.shine.com/job-search/">Search</a>
+                </li>
+                <li class="tab">
+                  <a href="https://www.shine.com/job-search/">Jobs For You</a>
+                </li>
+                <li class="tab">
+                  <a href="https://www.shine.com/free-job-alerts/">
+                    Create Alert
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class="tab-link" href="https://resume.shine.com/">
+                <span class="tab-label">
+                  <i class="iconH-job-assistance"></i>Job seeking assistance
+                </span>
+              </a>
+              <input type="checkbox" id="chck3" checked="" />
+              <label class="tab-arrow" for="chck3"></label>
+              <ul class="tab-content">
+                <li class="tab">
+                  <a href="https://resume.shine.com/product/profile-booster/4561">
+                    Profile Booster
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="https://resume.shine.com/product/application-highlighter-3/4117/">
+                    Application Highlighter
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="https://resume.shine.com/product/entry-level/1922/">
+                    Resume Writing
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="https://resume.shine.com/resume-score-checker/">
+                    Check Your Resume
+                  </a>
+                </li>
+                <li class="tab">
+                  <a class="link-color" href="https://resume.shine.com/listing">
+                    View all
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class="tab-link" href="index.html">
+                <span class="tab-label">
+                  <i class="iconH-courses"></i>Courses
+                </span>
+              </a>
+              <input type="checkbox" id="chck4" checked="" />
+              <label class="tab-arrow" for="chck4"></label>
+              <ul class="tab-content">
+                <li class="tab">
+                  <a href="search/results/indexbedf.html?farea=17">
+                    Sales and Marketing
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="search/results/index6e13.html?farea=22">
+                    Information and Technology
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="search/results/index59c4.html?farea=19">
+                    Operation Management
+                  </a>
+                </li>
+                <li class="tab">
+                  <a href="search/results/index1897.html?farea=25">
+                    Human Resources
+                  </a>
+                </li>
+                <li class="tab">
+                  <a class="link-color" href="search/results8635.html?all=true">
+                    View all
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="tabs--heading">
+              <a href="user-intent.html">
+                <i class="iconH-career-guidance"></i>Career Guidance
+              </a>
+            </li>
+          </ul>
+          <ul class="site-links">
+            <li>
+              <a href="talenteconomy/index8725.html?utm_source=www.shine.com&amp;utm_medium=searchdefault&amp;utm_campaign=msite_links">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="https://www.shine.com/aboutus/">About Us</a>
+            </li>
+            <li>
+              <a href="contact-us.html">Contact Us</a>
+            </li>
+            <li>
+              <a href="https://www.shine.com/faqs/">FAQs</a>
+            </li>
+            <li>
+              <a>Download</a>
+            </li>
+            <li>
+              <a href="https://www.shine.com/contactus/?type=reportJobPosting/">
+                Report a Job Posting
+              </a>
+            </li>
+          </ul>
+          <ul class="other-links">
+            <li>
+              <span class="other-links--left">Employers</span>
+              <span class="other-links--right">
+                <a href="https://recruiter.shine.com/?utm_source=shine&amp;utm_medium=footer&amp;utm_campaign=Candidate">
+                  Recruiter India
+                </a>
+                <span class="px-5">|</span>
+                <a href="https://recruiter.shine.com/online-package/offer/free-job-posting/">
+                  Post Jobs
+                </a>
+              </span>
+            </li>
+            <li>
+              <span class="other-links--left">Partner Site</span>
+              <span class="other-links--right">
+                <a href="https://www.ottplay.com/?utm_source=shine&amp;utm_medium=partner-site&amp;utm_campaign=april-21">
+                  OTTplay.com
+                </a>
+              </span>
+            </li>
+            <li>
+              <span class="other-links--left">Contact us</span>
+              <span class="other-links--right">
+                <a class="d-block" href="tel:080-4710 5555">
+                  080-4710 5555
+                </a>
+                <a href="mailto:contactus@shine.com" class="d-block">
+                  contactus@shine.com
+                </a>
+              </span>
+            </li>
+            <li>
+              <span class="other-links--left">Follow us</span>
+              <span class="other-links--right d-flex smo mt-0">
+                <a
+                  href="https://www.linkedin.com/company/shinecom/"
+                  class="smoLinkedin"
+                ></a>
+                <a
+                  href="https://twitter.com/Shinedotcom?lang=en"
+                  class="smotwitter"
+                ></a>
+                <a
+                  href="https://www.facebook.com/shinedotcom/"
+                  class="smofacebook"
+                ></a>
+              </span>
+            </li>
+          </ul>
+          <div class="slideMenu__footer">
+            <p>
+              ©{/* <!-- -->2022<!-- --> */}
+              HT Media Limited
+            </p>
+            <p class="slideMenu__footer--link">
+              <a href="privacy-policy/index.html">Privacy Policy</a>
+              <a href="https://www.shine.com/cookie-policy/">Cookie Policy</a>
+              <a href="https://www.shine.com/securityadvice/">Fraud Alert</a>
+              <a href="https://www.livemint.com/">Business News</a>
+              <a href="https://www.hindustantimes.com/">English News</a>
+              <a href="disclaimer/index.html">Disclaimer</a>
+              <a href="tnc/index.html">Terms &amp; Conditions</a>
+            </p>
+          </div>
+        </nav>
+        <div class="slideMenuOverlay"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
