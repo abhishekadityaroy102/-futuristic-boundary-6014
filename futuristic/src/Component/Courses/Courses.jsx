3be7f0228ff51bf415bc7e8d9866b12e76dc6f1f
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-28 18:39:06 +0530

    Update Courses.jsx

--- futuristic/src/Component/Courses/Courses.jsx
+++ futuristic/src/Component/Courses/Courses.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      Courses
+      <div>Courses</div>
+      <div>Courses</div>
+      <div>Courses</div>
+      <div>Courses</div>
+    </div>
+  );
 };
 
 export default Courses;
