7b4c162e0ed2d6cb9fdf34830e20e3d9777eb3b9
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-28 17:32:04 +0530

    Update App.js

--- futuristic/src/App.js
+++ futuristic/src/App.js
@@ -1,8 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Courses from "./Component/Courses/Courses";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Courses />
+    </div>
+  );
 }
 
 export default App;
