1e4a0029b5417f68f4bb3d37d7b25ea8b7e4c81a
apoorvachikara <chikara02apoorva@gmail.com>
2022-11-16 19:40:13 +0530

    Mockdata

--- futuristic/src/Component/Homepage/useQuerydata.jsx
+++ futuristic/src/Component/Homepage/useQuerydata.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect,useState } from "react";
+
+// feedback:fp02_102     - Mock data should be moved out of component
 export const locationdata=["Bangalore","Kerela","Barh","Noida","Bihar","Karnatka"]
 export const experiencedata=["0 years","0 to 1years","1 to 4 years","3 years","4 years","5 years","6 years","7 years","8 years","9 years","10 years+"]
 export const useQuerydata=(query)=>{
